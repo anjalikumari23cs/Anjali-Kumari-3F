3194742b8a417c4f19e79a77e4810c4767811fac
anjalikumari23cs <anjalikumari93680@gmail.com>
2025-08-19 09:39:35 +0530

    Ques3 added

---
+++ Day2/ques3.java
@@ -0,0 +1,12 @@
+package Day2;
+
+public class ques3 {
+    void display(){
+        int a=10;
+        System.out.println("a: "+a);
+    }
+    public static void main(String[] args) {
+        ques3 s=new ques3();
+        s.display();
+    }
+}

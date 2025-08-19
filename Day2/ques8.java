88293422a69fcc3796e14316df87b4bb52855fe1
anjalikumari23cs <anjalikumari93680@gmail.com>
2025-08-19 10:43:29 +0530

    Question 8

---
+++ Day2/ques8.java
@@ -0,0 +1,12 @@
+package Day2;
+
+public class ques8 {
+    static int count;
+    static{
+        System.out.println("static block");
+        count=10;
+    }
+    public static void main(String[] args) {
+        System.out.println("count is "+count);
+    }
+}

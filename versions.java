02db680ed1d1ccd7c3b45703e2c3ead660dd97bd
anjalikumari23cs <anjalikumari93680@gmail.com>
2025-08-19 08:53:04 +0530

    [Q1 added]

--- versions.java
+++ versions.java
@@ -1,6 +1,19 @@
-
+class Car{
+    String model;
+    int year;
+    Car(String model,int year){
+        this.model=model;
+        this.year=year;
+    }
+    void display(){
+        System.out.println("model:"+model+"year: "+year);
+    }
+}
 public class versions{
     public static void main(String[] args) {
-        
+        Car c1=new Car("Honda Civic",2022);
+        Car c2=new Car("Apache",2021);
+        c1.display();
+        c2.display();
     }
 }

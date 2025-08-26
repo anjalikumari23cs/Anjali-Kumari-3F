4eeabd33eff67b9a059f14a21097123ab2dc2faa
anjalikumari23cs <anjalikumari93680@gmail.com>
2025-08-26 09:35:31 +0530

    Added files from Day2 and Day3

---
+++ Day2/ques16.java
@@ -0,0 +1,8 @@
+package Day2;
+
+public class ques16 {
+    public static void main(String[] args) {
+        ques16 q=new ques16();
+        
+    }
+}
---
+++ Day3/arr8.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class arr8 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[10];
+        int maxindex;
+        int index=0;
+        for(int i=1;i<=n;i++){
+            arr[index]=0;
+        }
+    }
+}

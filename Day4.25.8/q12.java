069f5c13ca44a7b80b2429f7e7f1959fb4e210aa
anjalikumari23cs <anjalikumari93680@gmail.com>
2025-08-26 09:54:03 +0530

    Remove All digits from String

---
+++ Day4.25.8/q12.java
@@ -0,0 +1,11 @@
+import java.util.*;
+// Remove all digits from a string.
+public class q12 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        String res=s.replaceAll("\\d", "");
+        System.out.println(res);
+
+    }
+}

e8fbfec028b7937d883622d40799070381ad5015
anjalikumari23cs <anjalikumari93680@gmail.com>
2025-09-03 08:48:06 +0530

    Reverse a String

--- javaStringproblem/ReverseString.java
+++ javaStringproblem/ReverseString.java
@@ -1,7 +1,7 @@
 package javaStringproblem;
 
 import java.util.*;
-public class ReverseString {
+public class ReverseString{
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         String str=sc.nextLine();

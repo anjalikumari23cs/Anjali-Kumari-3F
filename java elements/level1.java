9e250e65e7448165b7d4faac4ee984a703d7e349
anjalikumari23cs <anjalikumari93680@gmail.com>
2025-09-17 09:39:37 +0530

    Java Elements level1

---
+++ java elements/level1.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class level1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int birth=2000;
+        int curr=2024;
+        int age=curr-birth;
+        System.out.println("Harry's age in 2024 is "+age);
+    }
+}

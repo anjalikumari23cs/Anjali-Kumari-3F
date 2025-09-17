8b97bf188f2cfc18a8b5b9704df3ee8978adf474
anjalikumari23cs <anjalikumari93680@gmail.com>
2025-09-17 09:54:38 +0530

    Modified level1 with q2

--- java elements/level1.java
+++ java elements/level1.java
@@ -1,10 +1,21 @@
-import java.util.*;
+// q1
+// import java.util.*;
+// public class level1 {
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         int birth=2000;
+//         int curr=2024;
+//         int age=curr-birth;
+//         System.out.println("Harry's age in 2024 is "+age);
+//     }
+// }
+
 public class level1 {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int birth=2000;
-        int curr=2024;
-        int age=curr-birth;
-        System.out.println("Harry's age in 2024 is "+age);
+        int math=94;
+        int physics=95;
+        int chemistry=97;
+        int avg=(math+physics+chemistry)/3;
+        System.out.println("Average marks of Sam are: "+avg);
     }
 }

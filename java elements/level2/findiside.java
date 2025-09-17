acb34173a156aeb0fd85d84551a263ee57fe188b
anjalikumari23cs <anjalikumari93680@gmail.com>
2025-09-17 10:20:17 +0530

    q3

---
+++ java elements/level2/findiside.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class findiside {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int p=sc.nextInt();
+        int s=p/4;
+        System.out.println("Side of squares are: "+s);
+    }
+}
